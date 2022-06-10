8ed401d2696871de904c84d9b7e8a5e2c4877435
Josep Bello <bellocontacto@gmail.com>
2022-06-10 18:34:42 +0200

    Modified line to get time

--- src/create_csv.sh
+++ src/create_csv.sh
@@ -4,7 +4,7 @@ echo "file_name;time;procs_per_node;nodes"
 for file in out/*
 do
     file_name=$(basename $file | cut -f 1 -d .)
-    time=$(grep elapsed $file | cut -f 3 -d ' ' | cut -f 1 -d 'e')
+    time=$(grep real $file | cut -f 2 -d ' ')
     procs=$(echo $file_name | cut -f 1 -d _)
     nodes=$(echo $file_name | cut -f 2 -d _)
     echo "$file_name;$time;$procs;$nodes"
